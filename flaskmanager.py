8cf1cd442ea824f12565850a056d142110b6f00a
BABU JOHN <bbuj27@gmail.com>
2018-10-26 10:04:07 +0000

    Changed Mongo Design

--- flaskmanager.py
+++ flaskmanager.py
@@ -1,14 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 import os
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get('MONGO_DBNAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+
+mongo = PyMongo(app)
+
 @app.route("/")
 def show_hi():
-    return "Hi"
+    tasks = mongo.db.tasks.find()
+    return render_template("tasks.html", tasks=tasks)
 
+@app.route("/tasks/<collection_name>")
+def get_tasks_by_collection(collection_name):
+    tasks = mongo.db[collection_name].find()
+    return render_template("tasks.html", tasks=tasks)
+
+
+@app.route("/add_task", methods=["GET", "POST"])
+def add_task():
+    if request.method=="POST":
+        form_values = request.form.to_dict()
+        category = form_values["category_name"]
+        mongo.db[category].insert_one(form_values)
+        return redirect("/")
+    else:
+        categories = []
+        for category in mongo.db.collection_names():
+            if not category.startswith("system."):
+                categories.append(category)
+        
+        return render_template("addtask.html", categories=categories)
+        
+        
 if __name__ == "__main__":
         app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
+
 
 
 
